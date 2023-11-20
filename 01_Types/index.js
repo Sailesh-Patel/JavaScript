7257d9c1d5047f0797a61a0faf8f5b112df51031
Sailesh-Patel1 <Sailesh_patel_bsc@hotmail.co.uk>
2023-11-20 11:49:40 +0000

    initial push to github

---
+++ 01_Types/index.js
@@ -0,0 +1,56 @@
+let age = 29;
+
+//log to print
+// this just ISES the value of age
+console.log(age);
+
+// to change the value in the vairable use =
+// need to use '=' to CHNAGE the value of age
+age = 30;
+
+console.log(age);
+
+// use ' or " to add text 
+let name = 'Sailesh'
+
+console.log(name)
+
+name = 'Streets'
+
+console.log(name)
+
+// false is a special key word which can not be used for anything else
+let muppet = false;
+
+console.log(muppet);
+
+muppet = true;
+
+console.log(muppet)
+
+let demo
+// underfined as NEVER HAD A VALUE
+console.log(demo)
+
+demo = null;
+
+console.log(demo)
+// use typeof to interrogate what type a variable is
+console.log(typeof muppet)
+
+age = 'Patel'
+
+console.log(age);
+
+//Strech 
+age = 31
+
+console.log(age);
+
+const PI = 3.14;
+
+console.log(PI);
+
+PI = 3.16
+
+console.log(PI);
